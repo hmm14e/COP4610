64e92ca2668379351a0afef7930e699b57204169
William Wagner <wcwwagner@gmail.com>
2018-02-08 10:53:27 -0500

    Add builtins boiler-plate

---
+++ builtins.c
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+int sh_cd(char ** args)
+{
+    return 0;
+}
+
+
+/**
+ *
+ */
+double sh_etime(char **args)
+{
+    return 0.0;
+}
+
+
+/**
+ *
+ */
+void sh_exit(char **args)
+{
+
+}
+
+
+/**
+ *
+ */
+void sh_echo(char **args)
+{
+
+}
+
+
+/**
+ * don't know the return type for this one
+ */
+void sh_io(char **args)
+{
+
+}
---
+++ builtins.h
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+int sh_cd(char ** args);
+
+
+/**
+ *
+ */
+double sh_etime(char ** args);
+
+
+/**
+ *
+ */
+void sh_exit(char ** args);
+
+
+/**
+ *
+ */
+void sh_echo(char ** args);
+
+
+/**
+ * don't know the return type for this one
+ */
+void sh_io(char ** args);
