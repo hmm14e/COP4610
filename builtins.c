7467a001af772500cbfb6ec8b449e4f3b85a8921
William Wagner <wcwwagner@gmail.com>
2018-02-11 22:26:48 -0500

    Implement etime fork

--- builtins.c
+++ builtins.c
@@ -25,34 +25,44 @@ int sh_cd(char ** args)
 }
 
 
-/**
- *
- */
+/* given input ["etime", "cmd", "arg1", ..., NULL], execute <cmd> and print time elapsed */
 int sh_etime(char **args)
 {
-    /* clip etime off command
-        args now holds just the cmd
-     */
+    /* clip etime off command, args now holds just the cmd */
     int i = 0;
     char ** args_cpy = strstr_copy(args + 1);
     struct timeval start, end, difference;
 
     gettimeofday(&start, NULL);
-    /* exec command */
-    gettimeofday(&end, NULL);
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("failed to fork");
+        exit(1);
+    }
+    else if (pid == 0) {
+        execv(args_cpy[0], args_cpy);
+        perror("sh: etime exec failure");
+        exit(1);
+    }
+    else {
+        /* block waiting for child to execute */
+        waitpid(pid, 0, 0);
+        gettimeofday(&end, NULL);
+    }
 
     /* calculate difference */
 
     difference.tv_sec = end.tv_sec - start.tv_sec;
     difference.tv_usec = end.tv_usec - start.tv_usec;
 
-    if(differecne.tv_usec < 0){
+    if(difference.tv_usec < 0){
         difference.tv_sec--;
         difference.tv_usec += 1000000;
     }
 
     _free2d(args_cpy);
-    printf("%f \n", ((double)difference.tv_sec + ((double)difference.tv_usec/1000000.0)));
+    printf("Elapsed time: %f\n", ((double)difference.tv_sec + ((double)difference.tv_usec/1000000.0)));
     return 1;
 }
 
@@ -123,7 +133,6 @@ int sh_execute_builtin(char **args)
     size_t num_builtins = sizeof(builtin_func_names) / sizeof(builtin_func_names[0]);
     for (int i = 0; i < num_builtins; i++) {
         if (strcmp(args[0], builtin_func_names[i]) == 0){
-            printf("calling built-in: %s\n", args[0]);
             return (*builtin_funcs[i])(args);
         }
     }
